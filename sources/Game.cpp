2ad1efec3cc7a2a90f49f15fb1e6135319051de0
Avichay3 <aviaimzina312@gmail.com>
2023-04-07 17:05:09 +0300

    another commit

--- sources/Game.cpp
+++ sources/Game.cpp
@@ -1,6 +1,9 @@
 #include "sources/game.hpp"
 #include "player.hpp"
 #include "iostream"
+#define SIZE_PACKET 52
+using namespace ariel;
+using namespace std;
 
 Game::game(Player p1, Player p2){
     this.
