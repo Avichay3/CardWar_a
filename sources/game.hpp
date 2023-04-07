3a2dd547c114bbf033ce8ea740f310518704af87
Avichay3 <aviaimzina312@gmail.com>
2023-04-07 16:39:52 +0300

    commit

--- sources/game.hpp
+++ sources/game.hpp
@@ -1,18 +1,27 @@
+#include "iostream"
+#include "player.hpp"
 using namespace ariel;
 
 class Game{
-    player p1;
-    player p2;
+    Player p1;
+    Player p2;
 
     public:
-        Game(player p1, player p2){}; //constructor for the class
-        
+    Game(Player& player1, Player& player2): p1(player1) , p2(player2) {}
+            
+            
+
+        Game(Player p1, Player p2){}; //constructor for the class
+
         void playTurn();
         void playAll();
         void printWiner();
         void printStats();
         void printLog();
         void printLastTurn();
+        Player& getPlayerP1(){return p1}; //getter
+        Player& getPlayerP2(){return p2}; //getter
+
 }
 
 
